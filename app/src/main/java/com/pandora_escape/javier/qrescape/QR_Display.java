dda753cab2bccee9a0a59ed0bddf4daca0e4c4c0
Javiergg <jgg.correobasura@gmail.com>
2015-06-27 16:29:51 +0800

    Initial commit

---
+++ app/src/main/java/com/pandora_escape/javier/qrescape/QR_Display.java
@@ -0,0 +1,54 @@
+package com.pandora_escape.javier.qrescape;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class QR_Display extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = getLayoutInflater().inflate(R.layout.activity_qr_display, null);
+
+        TextView titleView = (TextView) view.findViewById(R.id.titleTextView);
+        TextView bodyView = (TextView) view.findViewById(R.id.bodyTextView);
+
+        String title = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE_TITLE);
+        String body = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE_BODY);
+
+        titleView.setText(title);
+        bodyView.setText(body);
+
+        // Set the text view as the activity layout
+        setContentView(view);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_qr__display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
