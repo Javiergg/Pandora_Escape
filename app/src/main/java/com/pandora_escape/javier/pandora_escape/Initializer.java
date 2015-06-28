95d7defd97b53d43c993a8df474bf9ff2d8651a3
Javiergg <jgg.correobasura@gmail.com>
2015-06-29 03:24:02 +0800

    Clean code. Change Message index from int to Integer. An incorrect message has index=null

--- app/src/main/java/com/pandora_escape/javier/pandora_escape/Initializer.java
+++ app/src/main/java/com/pandora_escape/javier/pandora_escape/Initializer.java
@@ -3,17 +3,14 @@ package com.pandora_escape.javier.pandora_escape;
 import android.app.Activity;
 import android.app.TaskStackBuilder;
 import android.content.Intent;
-import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class SortingHat extends Activity {
+public class Initializer extends Activity {
 
     public void sendMessage(Message message){
         Intent messageIntent = new Intent(this,QR_Display.class);
@@ -31,37 +28,17 @@ public class SortingHat extends Activity {
             intentToMain.putExtra(MainActivity.EXTRA_ADD_MESSAGE,message.index);
         }
 
-        if(!Message.messageList.contains(message)){
-            Message.messageList.add(message);
-        }
-
         TaskStackBuilder.create(this)
                 .addNextIntentWithParentStack(intentToMain)
                 .addNextIntentWithParentStack(messageIntent)
                 .startActivities();
-
-        //startActivity(messageIntent);
         finish();
     }
-
-
-    public void parsePandoraUri(Uri uri){
-        if(uri==null) {
-            return;
-        }
-
-        String code = uri.getQueryParameter(getString(R.string.pandora_uri_query_code));
-        if(code!=null){
-            sendMessage(new Message(code));
-        }
-    }
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_sorting_hat);
 
         Message.initialize(getApplicationContext());
 
@@ -71,42 +48,26 @@ public class SortingHat extends Activity {
 
             Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             if (rawMsgs != null) {
-
+                Message message = null;
                 for (Parcelable rawMsg : rawMsgs) {
                     NdefRecord[] records = ((NdefMessage) rawMsg).getRecords();
 
                     for (NdefRecord record : records) {
-                        parsePandoraUri(record.toUri());
+                        message = Message.parsePandoraUri(record.toUri());
+                        if(message!=null){ break; }
                     }
+                    if(message!=null){ break; }
+                }
+
+                if(message!=null){
+                    sendMessage(message);
                 }
             }
+
         }else{
             startActivity(new Intent(this,MainActivity.class));
             finish();
         }
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sorting_hat, menu);
-        return true;
-    }
-    */
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
